a517362fd4a0a521ae0a35aefb98a55120ec2685
lucianamihai <luciana.mihai@student.unitbv.ro>
2025-01-12 23:22:07 +0200

    ShoppingCart display of ingredients list

---
+++ app/src/main/java/com/example/flavormix/adapters/IngredientAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.flavormix.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flavormix.databinding.ItemIngredientBinding
+import com.example.flavormix.model.Ingredient
+
+class IngredientAdapter(private val onDeleteClick: (Ingredient) -> Unit) : ListAdapter<Ingredient, IngredientAdapter.IngredientViewHolder>(IngredientDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        val binding = ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return IngredientViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class IngredientViewHolder(private val binding: ItemIngredientBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(ingredient: Ingredient) {
+            binding.ingredientNameTextView.text = ingredient.name
+
+            binding.ingredient = ingredient
+            binding.deleteButton.setOnClickListener {
+                onDeleteClick(ingredient)
+            }
+            binding.executePendingBindings()
+        }
+    }
+
+    class IngredientDiffCallback : DiffUtil.ItemCallback<Ingredient>() {
+        override fun areItemsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Ingredient, newItem: Ingredient): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
